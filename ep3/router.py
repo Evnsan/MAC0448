a845c7c41ae8deb10d129598798aae876819fbf7
Pedro Ferreira Alexandre <pfa@snake.linux.ime.usp.br>
2015-11-17 22:14:36 -0200

    alterada funcoes cmdhost e cmdrouter com funcionalidades basicas. Foi feito a cmdPerformance, setando os ips e a quantdade de pacotes por portas nos roteadores

---
+++ ep3/router.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+class Router(object):
+	def __init__(self, nome, numDeInterfaces):
+		self.nome = nome
+		self.numDeInterfaces = numDeInterfaces
+		self.enlaces = []
+		self.tempoPacote = None
+		self.portas = {}
+		super(Router, self).__init__()
+
+	def setEnlace(se,fposEnlace, enlace):
+		self.enlaces[posEnlace] = enlace
+
+	def setPortas(self,args):
+		for i in xrange(0,2*int(self.numDeInterfaces),2):
+			self.portas[args[i]] = args[i+1] 
+		
+	def setTempoPacote(self,tempo):
+		self.tempoPacote = tempo
+
+
+##getters e setters
+
+	def getNome(self):
+		return self.nomeDoRouter
+	
--- ep3/universo.py
+++ ep3/universo.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 
 from parser import Parser
+from router import Router
+from host import Host
 
-
+elementos = {}
+routers = {}
+hosts = {}
 
 
 ## Comandos secundarios
 
 def cmdRouter(args):
 	print "cmdRouter" + str(args)
+	r = Router(args[1], args[2])
+	routers[args[1]] = r
+	elementos[args[1]] = r
 
 
 def cmdHost(args):
 	print "cmdHost" + str(args)
+	h = Host(args[1])
+	hosts[args[1]] = h
+	elementos[args[1]] = h
 
 def cmdDuplexLink(args):
 	print "cmdDuplexLink" + str(args)
@@ -22,7 +32,12 @@ def cmdIp(args):
 
 def cmdPerformance(args):
 	print "cmdPerformance" + str(args)
-
+	del args[0]
+	r = routers[args[0]]
+	del args[0]
+	r.setTempoPacote(args[0])
+	del args[0]
+	r.setPortas(args)
 def cmdIrcc(args):
 	print "cmdIrcc" + str(args)
 
@@ -61,17 +76,12 @@ comandosSimulate = {}
 
 def executaComandos(cmds):
 	for cmd,args in cmds:
-	
 		comandos[cmd](args)
 
 def main():
 	p = Parser()
 	cmds = p.lerComandos()
 	executaComandos(cmds)
-
-
-
-
 
 
 
