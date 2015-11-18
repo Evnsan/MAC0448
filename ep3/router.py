7d73c7ec7792626953dd17cccfbcc5415d9add26
Pedro Ferreira Alexandre <pfa@lisp.linux.ime.usp.br>
2015-11-18 21:28:36 -0200

    montando objeto portas no router

--- ep3/router.py
+++ ep3/router.py
@@ -10,11 +10,14 @@ class Router(object):
         self.portas = []
         self.sniffer = None
         self.rotas = {} 
+        self.passosRestantes = 0
+        self.cmdaRedes = CamadaRedes()
         super(Router, self).__init__()
         for i in range(numDeInterfaces):
             self.enlaces.append(None)
             self.ips.append(None)
             self.portas.append(None)
+
 
     def __repr__(self):
         return "ROUTER: " + str(self.nome) + " IPS(" + str(self.ips) + ")"
@@ -25,17 +28,26 @@ class Router(object):
     def getRota(self, remetente):
         return rotas[remetente]
 
-    def setEnlace(self, porta, enlace):
-        self.enlaces[porta] = enlace
+    def setEnlace(self, numporta, enlace):
+        self.portas[numporta].setEnlace(enlace)
+        
 
     def setPortas(self,args):
         for i in xrange(0,2*int(self.numDeInterfaces),2):
-            self.portas[args[i]] = args[i+1] 
+            self.portas[args[i]].setTamanhoBuffer(args[i+1]) 
 		
     def setTempoPacote(self,tempo):
         self.tempoPacote = tempo
     
     def passo(self, relogio):
+        if passos == 0:
+            self.cmdaRedes.
+            pass
+            #processarPacote
+
+        else:    
+            passosRestantes -= 1
+
         print "ROUTER(" + self.nome + "): Meu turno"
 
     def setSniffer(self, porta, sniffer):
@@ -44,7 +56,7 @@ class Router(object):
     
     def setIp(self, args):
         for i in xrange(0,2*int(self.numDeInterfaces),2):
-            self.ips[int(args[i])] = args[i+1] 
+            self.portas[int(args[i])].setIp(args[i+1]) 
         
 ##getters e setters
 
