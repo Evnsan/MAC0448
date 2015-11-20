1d511f95bdbe49f3683ef9e8ad6ef2ad33e67de1
Pedro Ferreira Alexandre <pfa@assembly.linux.ime.usp.br>
2015-11-19 22:32:23 -0200

    incluindo arquivo ircc

---
+++ ep3/ircc.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+from ircc import Ircc
+
+
+class Ircc(object):
+    def __init__(self, camadaTransporte, portaOrigem):
+        self.servidorIp = None
+        self.servidorPorta = None
+        self.nomeUsuario = None
+        self.estado = None
+        self.bloqueado = False
+        self.buffer = []
+        self.camadaTransporte = camadaTransporte
+        self.portaOrigem = portaOrigem
+        super(Ircc, self).__init__()
+
+
+    def passo3(self):
+        while len(self.buffer > 0) and not self.bloqueado:
+            args = self.buffer[0]
+            del self.buffer[0]
+            cmd = args[0]
+            del args[0]
+            comandos[cmd](args)
+
+
+    estado = {'CONECTADO' ,'NAOCONECTADO', 'LOGADO'}
+
+    
+    def cmdConnect(self,args):
+        temp = args[0].split(".")
+        if len(temp) > 1: 
+            self.servidorIp = args[0]
+        else:
+            #fazer chamada DNS
+            self.bloqueado = True
+        self.servidorPorta = args[1]
+        self.envia(m)
+
+    def cmdUser(self, args):
+        m = Mensagem()
+        m.setMsg(args+ " 8 *")
+        self.envia(m)
+    
+    def cmdQuit(self, args):
+        m = Mensagem()
+        m.setMsg(args)
+        self.envia(m)
+
+    def envia(self,m):
+        self.camadaTransporte.empacotaSegmento(m)
+
+
+    comandos = {'CONNECT': cmdConnect,
+                'USER': cmdUser,
+                'QUIT': cmdQuit}
+
+    def realizaRotina(self, comando):
+        comando = comandos[0]
+        del comandos[0]
+        retorno = comandos[comando](comandos[0])
+        return retorno  
+
+    def recebeMensagem(self, )   
+        
+
+
+    def cmdConnect(args):
+        retorno.append(args[0])
+        retorno.append(args[1])
+        retorno.append(args[2])
+        return retorno
+        
+    def cmdUser(args):
+        self.args[1] = 
+
+    def cmdFinish(args):
